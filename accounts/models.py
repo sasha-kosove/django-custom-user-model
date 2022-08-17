ec17ded3e0909a3314f6f1cc28d95e1a6cbd9b7a
sasha-kosove <alexander.kosove@gmail.com>
2022-08-17 21:36:43 +0300

    Added methods to a User Model

--- accounts/models.py
+++ accounts/models.py
@@ -39,5 +39,16 @@ class Account(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
+    class Meta:
+        verbose_name = _("account")
+        verbose_name_plural = _("accounts")
+
     def __str__(self):
         return self.email
+
+    def get_full_name(self):
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip() or self.email.split("@")[0]
+
+    def get_short_name(self):
+        return self.first_name or self.email.split("@")[0]
